638e341b821df22511b4ac5580e7e199cd8360f5
umtong <dddgjj03@gmail.com>
2026-08-08 20:21:56 +0900

    candidate25: add import adapter

---
+++ research/candidate-25/candidate25_strategy.py
@@ -0,0 +1,5 @@
+"""Import adapter for Candidate 25."""
+from funding_window_strategy import Candidate25Config
+from funding_window_strategy import Candidate25Strategy
+
+__all__ = ["Candidate25Config", "Candidate25Strategy"]
