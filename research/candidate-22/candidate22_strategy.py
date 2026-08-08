89041160f747858eef462a14dfaf837c45a84147
umtong <dddgjj03@gmail.com>
2026-08-08 19:42:22 +0900

    candidate22: add import adapter

---
+++ research/candidate-22/candidate22_strategy.py
@@ -0,0 +1,5 @@
+"""Import adapter for Candidate 22."""
+from session_expansion_strategy import Candidate22Config
+from session_expansion_strategy import Candidate22Strategy
+
+__all__ = ["Candidate22Config", "Candidate22Strategy"]
