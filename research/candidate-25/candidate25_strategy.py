19e28d85d4839da6a8f611b4f0dfe5a5521276c1
umtong <dddgjj03@gmail.com>
2026-08-08 20:41:17 +0900

    candidate25: load state-first reset-leg strategy

--- research/candidate-25/candidate25_strategy.py
+++ research/candidate-25/candidate25_strategy.py
@@ -1,5 +1,5 @@
 """Import adapter for Candidate 25."""
-from isolated_funding_window_strategy import Candidate25Config
-from isolated_funding_window_strategy import Candidate25Strategy
+from state_first_geometry_strategy import Candidate25Config
+from state_first_geometry_strategy import Candidate25Strategy
 
 __all__ = ["Candidate25Config", "Candidate25Strategy"]
