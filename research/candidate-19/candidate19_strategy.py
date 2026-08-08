192179ecb210dc45230f1cdd5a78e35dd1c063dd
umtong <dddgjj03@gmail.com>
2026-08-08 18:52:34 +0900

    candidate19: add import adapter

---
+++ research/candidate-19/candidate19_strategy.py
@@ -0,0 +1,5 @@
+"""Import adapter for Candidate 19."""
+from transmission_strategy import Candidate19Config
+from transmission_strategy import Candidate19Strategy
+
+__all__ = ["Candidate19Config", "Candidate19Strategy"]
