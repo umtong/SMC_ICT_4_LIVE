4a0202327995fc265a3161993c4a156fb487c772
umtong <dddgjj03@gmail.com>
2026-08-10 21:32:41 +0900

    candidate57: add private ichi5m runtime facade

---
+++ research/candidate-57/router_ichi5m_private_runtime.py
@@ -0,0 +1,3 @@
+"""Runtime facade for the private ichi5m reconstruction."""
+from router_ichi5m_private_impl import *  # noqa: F401,F403
+from router_picasso import _aggregate_complete, _atr, _ema, _finite  # noqa: F401
