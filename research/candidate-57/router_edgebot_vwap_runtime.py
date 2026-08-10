ab0e1574b7c030b08889ce232b02bd557537c9c2
umtong <dddgjj03@gmail.com>
2026-08-10 22:21:45 +0900

    candidate57: add EdgeBot VWAP runtime facade

---
+++ research/candidate-57/router_edgebot_vwap_runtime.py
@@ -0,0 +1,3 @@
+"""Runtime facade for EdgeBot 4σ rolling-VWAP reconstruction."""
+from router_edgebot_vwap_impl import *  # noqa: F401,F403
+from router_picasso import _aggregate_complete, _atr, _ema, _finite  # noqa: F401
